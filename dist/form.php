80a7229b376cdf6257ada2b9fb489e541688660c
gondigesu <godigesu@gmail.com>
2020-08-12 21:07:08 -0300

    formulario php

---
+++ dist/form.php
@@ -0,0 +1,201 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/autoload.php';
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->Username = 'gdigesu@contabledg.com.ar';
+    $mail->Password = 'Laspada300488';
+    $mail->setFrom('gdigesu@contabledg.com.ar', 'Contable DG');
+    $mail->addAddress('godigesu@gmail.com', 'Gonzalo Di Gesu');
+    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+        $mail->Subject = 'PHPMailer contact form';
+        $mail->isHTML(false);
+        $mail->Body = <<<EOT
+Email: {$_POST['email']}
+Name: {$_POST['name']}
+Message: {$_POST['message']}
+EOT;
+        if (!$mail->send()) {
+            $msg = 'Algo anduvo mal, por favor volvé a internar en unos minutos.';
+        } else {
+            $msg = 'Mensaje enviado. Muchas gracias.';
+        }
+    } else {
+        $msg = 'Dejame tu consulta';
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Formulario de Contacto</title>
+</head>
+<body>
+<div class="container">  
+  <form id="contact" action="" method="POST">
+    <h3>Formulario de Contacto</h3>
+    <fieldset>
+      <input placeholder="Nombre" type="text" tabindex="1" required autofocus name="name">
+    </fieldset>
+    <fieldset>
+      <input placeholder="Correo Electrónico" type="email" tabindex="2" required name="email">
+    </fieldset>
+    <fieldset>
+      <textarea placeholder="Mensaje" tabindex="5" required name="message"></textarea>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Enviar</button>
+    </fieldset>
+    <fieldset>
+      <button name="cancel" type="cancel" id="contact-submit" data-submit="...Sending"><a href="index.html" class="linkatras">Cancelar</a></button>
+    </fieldset>
+  </form>
+</div>  
+</body>
+<style>
+@import url(https://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600);
+* {
+	margin:0;
+	padding:0;
+	box-sizing:border-box;
+	-webkit-box-sizing:border-box;
+	-moz-box-sizing:border-box;
+	-webkit-font-smoothing:antialiased;
+	-moz-font-smoothing:antialiased;
+	-o-font-smoothing:antialiased;
+	font-smoothing:antialiased;
+	text-rendering:optimizeLegibility;
+}
+
+body {
+	font-family:"Open Sans", Helvetica, Arial, sans-serif;
+	font-weight:300;
+	font-size: 12px;
+	line-height:30px;
+	color:#777;
+	background:black;
+}
+
+.linkatras{
+    color:white;
+    text-decoration:none;
+}
+
+.container {
+	max-width:400px;
+	width:100%;
+	margin:0 auto;
+	position:relative;
+}
+
+#contact input[type="text"], #contact input[type="email"], #contact input[type="tel"], #contact input[type="url"], #contact textarea, #contact button[type="submit"] { font:400 12px/16px "Open Sans", Helvetica, Arial, sans-serif; }
+
+#contact {
+	background:#F9F9F9;
+	padding:25px;
+	margin:50px 0;
+}
+
+#contact h3 {
+	color: #F96;
+	display: block;
+    font-size: 25px;
+    text-align:center;
+    font-weight: 400;
+    padding:20px;
+}
+
+fieldset {
+	border: medium none !important;
+	margin: 0 0 10px;
+	min-width: 100%;
+	padding: 0;
+	width: 100%;
+}
+
+#contact input[type="text"], #contact input[type="email"], #contact input[type="tel"], #contact input[type="url"], #contact textarea {
+	width:100%;
+	border:1px solid #CCC;
+	background:#FFF;
+	margin:0 0 5px;
+	padding:10px;
+}
+
+#contact input[type="text"]:hover, #contact input[type="email"]:hover, #contact input[type="tel"]:hover, #contact input[type="url"]:hover, #contact textarea:hover {
+	-webkit-transition:border-color 0.3s ease-in-out;
+	-moz-transition:border-color 0.3s ease-in-out;
+	transition:border-color 0.3s ease-in-out;
+	border:1px solid #AAA;
+}
+
+#contact textarea {
+	height:100px;
+	max-width:100%;
+  resize:none;
+}
+
+#contact button[type="submit"] {
+	cursor:pointer;
+	width:100%;
+	border:none;
+	background:#0CF;
+	color:#FFF;
+	margin:0 0 5px;
+	padding:10px;
+    font-size:15px;
+    height:41px;
+}
+
+#contact button[type="cancel"] {
+	cursor:pointer;
+	width:100%;
+	border:none;
+	background:red;
+	color:#FFF;
+	margin:0 0 5px;
+	padding:10px;
+    font-size:15px;
+    height:41px;
+    display: table-cell;
+    vertical-align: middle;
+}
+
+#contact button[type="submit"]:hover {
+	background:#09C;
+	-webkit-transition:background 0.3s ease-in-out;
+	-moz-transition:background 0.3s ease-in-out;
+	transition:background-color 0.3s ease-in-out;
+}
+
+#contact button[type="cancel"]:hover {
+	background:#09C;
+	-webkit-transition:background 0.3s ease-in-out;
+	-moz-transition:background 0.3s ease-in-out;
+	transition:background-color 0.3s ease-in-out;
+}
+
+#contact button[type="submit"]:active { box-shadow:inset 0 1px 3px rgba(0, 0, 0, 0.5); }
+
+#contact input:focus, #contact textarea:focus {
+	outline:0;
+	border:1px solid #999;
+}
+::-webkit-input-placeholder {
+ color:#888;
+}
+:-moz-placeholder {
+ color:#888;
+}
+::-moz-placeholder {
+ color:#888;
+}
+:-ms-input-placeholder {
+ color:#888;
+}
+</style>
+</html>
--- js/index.js
+++ js/index.js
@@ -30,10 +30,6 @@ function itemserviciosHandler(){
     modalservicios.style.display="block";
 }
 
-function contactoHandler(){
-    formulario.style.display='block';
-}
-
 function closebioHandler(){
     closebio.parentNode.parentNode.style.display="none";
 }
@@ -45,34 +41,6 @@ function closeserviciosHandler(){
     closeservicios.parentNode.parentNode.style.display="none";
 }
 
-function cancelarHandler(){
-    formulario.style.display='none';
-    nombreformulario.value="";
-    apellidoformulario.value="";
-    mailformulario.value="";
-    inputformulario.value="";
-}
-
-function validarMail(){
-    if((mailformulario.value.length!==0) && (mailformulario.value.indexOf !==('@') -1) && (mailformulario.value.indexOf('.') !== -1)){
-       mailformulario.classList.add('is-valid');
-       mailformulario.classList.remove('is-invalid');
-       borrarError();
-    } else {
-       mailformulario.classList.add('is-invalid');
-       mailformulario.classList.remove('is-valid');
-       crearError(`Por favor complete un correo electrónico válido.`)
-    }
-    validarBoton()
-}
-
-function validarBoton(){
-    if (nombreformulario.classList.contains('is-valid') && apellidoformulario.classList.contains('is-valid') && mailformulario.classList.contains('is-valid') && inputformulario.classList.contains('is-valid')){
-        botonenviar.removeAttribute('disabled')
-        botonenviar.setAttribute('enabled', 'true')
-    } else {botonenviar.setAttribute('disabled', 'true')
-        botonenviar.removeAttribute('enabled')}
-}
 
 function crearError(texto){
     const error=document.createElement('p');
@@ -104,8 +72,6 @@ function validarElemento(elemento){
     }
 }
 
-contacto.addEventListener('click', contactoHandler);
-botoncancelar.addEventListener('click', cancelarHandler);
 itembio.addEventListener('click', itembioHandler);
 itemrs.addEventListener('click', itemrsHandler);
 itemservicios.addEventListener('click', itemserviciosHandler);
@@ -113,8 +79,5 @@ closebio.addEventListener('click', closebioHandler);
 closers.addEventListener('click', closersHandler);
 closeservicios.addEventListener('click', closeserviciosHandler);
 
-validarElemento(nombreformulario);
-validarElemento(apellidoformulario);
-validarElemento(inputformulario);
-mailformulario.onblur=validarMail;
+
 
